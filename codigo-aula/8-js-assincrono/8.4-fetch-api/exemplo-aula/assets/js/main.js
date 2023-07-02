d3b65e67648953cf4ac3be5218bf8005a64a070d
Diego Santana <92051044+diesantana@users.noreply.github.com>
2023-07-02 15:14:21 -0300

    Substituindo XHR para Fetch

--- codigo-aula/8-js-assincrono/8.4-fetch-api/exemplo-aula/assets/js/main.js
+++ codigo-aula/8-js-assincrono/8.4-fetch-api/exemplo-aula/assets/js/main.js
@@ -1,8 +1,35 @@
-fetch(url, init)
-   .then(response => {
-      // Lidar com a resposta
-   })
-   .catch(error => {
-      // Lidar com erros 
-   });
+async function request(url) {
+   try {
+      const response = await fetch(url);
+      if (!response.ok) {
+         throw new Error(`OOPS! Temos um erro: ${response.status}`);
+      }
+      const dataText = await response.text();
+      getDados(dataText);  
+   } catch (error) {
+      console.log(error);
+   }
+}
+
+// Exibe os dados na página
+function getDados(dados) {
+   const div = document.querySelector('.conteudo-pag');
+   div.innerHTML = dados;
+}
+
+// Pegando o clique nos links
+document.addEventListener('click', event => {
+   let element = event.target;
+   let tagName = element.tagName.toLowerCase();
+   // verificando se o elemento clicado é um <a>
+   if (tagName === 'a') {
+      event.preventDefault();
+      // pegando o valor do atributo href do link clicado 
+      let href = element.getAttribute('href')
+      // chamando a função que vai fazer a requisição
+      request(href);
+   }
+})
+
+
 
