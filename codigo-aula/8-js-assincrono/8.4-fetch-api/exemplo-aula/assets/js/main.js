797c6c4c880753cfa730fcd1186e563fbe638339
Diego Santana <92051044+diesantana@users.noreply.github.com>
2023-07-02 11:59:41 -0300

    Fetch com async await

--- codigo-aula/8-js-assincrono/8.4-fetch-api/exemplo-aula/assets/js/main.js
+++ codigo-aula/8-js-assincrono/8.4-fetch-api/exemplo-aula/assets/js/main.js
@@ -1,39 +1,8 @@
-// function request(url) {
-//     fetch(url)
-//         .then(response => {
-//             if(!response.ok){
-//                 throw new Error(`HTTP error! Status: ${response.status}`)
-//             }
-//             return response.text()
-//         })
-//         .then(dados => getDados(dados))
-//         .catch(error => console.log(error));
-// }
+fetch(url, init)
+   .then(response => {
+      // Lidar com a resposta
+   })
+   .catch(error => {
+      // Lidar com erros 
+   });
 
-async function request(url) {
-    try {
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        const responseText = await response.text();
-        getDados(responseText);
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-document.addEventListener('click', event => {
-    let element = event.target;
-    let tagName = element.tagName.toLowerCase();
-    if (tagName === 'a') {
-        event.preventDefault();
-        let href = element.getAttribute('href');
-        request(href);
-    }
-})
-
-function getDados(dados) {
-    let div = document.querySelector('.conteudo-pag');
-    div.innerHTML = dados;
-}
