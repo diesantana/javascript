65c3aca2fb1fb95d017b10cb15814834dba47521
Diego Santana <92051044+diesantana@users.noreply.github.com>
2023-07-03 22:39:43 -0300

    Exercícios Fetch

---
+++ exercícios/12-fetch/ex01/script.js
@@ -0,0 +1,22 @@
+// Utilizando a API https://viacep.com.br/ws/${CEP}/json/
+// crie um formulário onde o usuário pode digitar o cep
+// e o endereço completo é retornado ao clicar em buscar
+
+async function request(cep) {
+    const cepTarget = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    const resposta = await cepTarget.json();
+    getCep(resposta);
+}
+
+function getCep(cepJson){
+    const elementoHTML = document.querySelector('.resposta');
+    elementoHTML.innerHTML = cepJson.logradouro;
+}
+
+document.addEventListener('submit', evento => {
+    evento.preventDefault();
+    const inputCep = document.querySelector('input');
+    const inputcepValue = inputCep.value;
+    request(inputcepValue);
+});
+
